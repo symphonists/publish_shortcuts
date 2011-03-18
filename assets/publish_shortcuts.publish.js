d6a70a11499b1e18151cb5cbab013e5b86ccf701
Nick Dunn <nick@nick-dunn.co.uk>
2011-03-18 09:02:52 +0000

    Substantial rewrite adopting Symphony 2.2 best practice: updated accessors, duplicator use, naming conventions, use of Symphony.Context JavaScript object

---
+++ assets/publish_shortcuts.publish.js
@@ -0,0 +1,24 @@
+var PublishShortcuts = {
+	
+	render: function() {
+		
+		var links = Symphony.Context.get('publish_shortcuts');
+		var container = jQuery('#contents h2:first .create:first');
+		
+		for(var i in links) {
+			container.after(
+				'<a href="' + this.format_url(links[i].link) + '" class="button publish-shortcut">' + links[i].label + '</a>'
+			);
+		}
+	},
+	
+	format_url: function(url) {
+		url = url.replace('{$root}', Symphony.Context.get('root'));
+		return url;
+	}
+	
+};
+
+jQuery(document).ready(function() {
+	PublishShortcuts.render();
+});
--- extension.driver.php
+++ extension.driver.php
@@ -1,256 +1,219 @@
 <?php
- 
-  require_once(TOOLKIT . '/class.sectionmanager.php');
-  
-  Class extension_publish_shortcuts extends Extension
-  {
-    private $_sectionManager;
-    
-    public function __construct($args)
-    {
-      $this->_Parent =& $args['parent'];
-      $this->_sectionManager = new SectionManager($this->_Parent);
-    }
-    
-    /*-------------------------------------------------------------------------
-      Extension definition
-    -------------------------------------------------------------------------*/
-    
-    public function about()
-    {
-      return array(
-        'name' => 'Publish Shortcuts',
-        'version' => '1.0.1',
-        'release-date' => '2010-11-04',
-        'author' => array(
-          'name' => 'Max Wheeler',
-          'email' => 'max@makenosound.com',
-        ),
-        'description' => 'Lets you define shortcut buttons for the section/publish index pages.',
-        'compatibility' => array(
-          '2.0.0' => false,
-          '2.0.1' => false,
-          '2.0.2' => false,
-          '2.0.3' => false,
-          '2.0.4' => false,
-          '2.0.5' => false,
-          '2.0.6' => false,
-          '2.0.7' => true
-        )
-      );
-    }
-    
-    /*-------------------------------------------------------------------------
-      Un/installation
-    -------------------------------------------------------------------------*/
-    
-    public function install() {
-      $this->_Parent->Database->query("
-        CREATE TABLE IF NOT EXISTS `tbl_publish_shortcuts` (
-          `id` int(11) NOT NULL auto_increment,
-          `label` varchar(255) NOT NULL,
-          `link` varchar(255) NOT NULL,
-          `section_id` int(11) NOT NULL,
-          `order` int(11) NOT NULL,
-          PRIMARY KEY (`id`)
-        )
-      ");
-    }
-    
-    public function uninstall() {
-      $this->_Parent->Database->query("DROP TABLE `tbl_publish_shortcuts`");
-    }
-     
-    /*-------------------------------------------------------------------------
-      Delegates
-    -------------------------------------------------------------------------*/
-    
-    public function getSubscribedDelegates()
-    {
-      return array(  
-        array(
-          'page'      => '/system/preferences/',
-          'delegate'  => 'AddCustomPreferenceFieldsets',
-          'callback'  => 'addCustomPreferenceFieldsets'
-        ),
-        array(
-          'page'      => '/backend/',
-          'delegate'  => 'InitaliseAdminPageHead',
-          'callback'  => 'initaliseAdminPageHead'
-          ),
-        array(
-          'page'      => '/system/preferences/',
-          'delegate'  => 'Save',
-          'callback'  => 'save'
-        )
-      );
-    }
-    
-    # Build the Preferences page
-    public function addCustomPreferenceFieldsets(&$context)
-    {
-      $sections = $this->getSections();
-      // Build section options array
-      foreach ($sections as $section) {
-        $options[] = array($section['id'], false, $section['name']);
-      }
-      
-      $fieldset = new XMLElement('fieldset');
-      $fieldset->setAttribute('class', 'settings');
-      $fieldset->appendChild(new XMLElement('legend', __('Publish Shortcuts')));
-       
-      $p = new XMLElement('p', __('Shortcut links for Section index pages'));
-      $p->setAttribute('class', 'help');
-      $fieldset->appendChild($p);
-      
-      $group = new XMLElement('div');
-      $group->setAttribute('class', 'subsection');
-      $group->appendChild(new XMLElement('h3', __('Shortcuts Links')));
-       
-      $ol = new XMLElement('ol');
-      $ol->setAttribute('id', 'publish_shortcuts');
-      
-      # Create the duplicator template
-      $li = new XMLElement('li');
-      $li->setAttribute('class', 'template');
-      $li->appendChild(new XMLElement('h4', __("Shortcut")));
-      
-      $divcontent = new XMLElement('div');
-      $divcontent->setAttribute('class', 'content');
-      
-      $divgroup = new XMLElement('div');
-      $divgroup->setAttribute('class', 'group');
-      
-      $label = Widget::Label(__('Label'));
-      $label->appendChild(Widget::Input("settings[publish_shortcuts][][label]", __("Label")));
-      $divgroup->appendChild($label);
-      
-      $label = Widget::Label(__('Link'));
-      $label->appendChild(Widget::Input("settings[publish_shortcuts][][link]", __("Link")));
-      $divgroup->appendChild($label);
-      $divcontent->appendChild($divgroup);
-      
-      $divgroup = new XMLElement('div');
-      $divgroup->setAttribute('class', 'group');
-      
-      $label = Widget::Label(__('Section'));
-      $label->appendChild(Widget::Select("settings[publish_shortcuts][][section_id]", $options));
-      $divgroup->appendChild($label);
-      $divcontent->appendChild($divgroup);
-      
-      $li->appendChild($divcontent);
-      $ol->appendChild($li);
-      
-      # Build up existing shortcuts
-      if($shortcuts = $this->getShortcuts()) {
-        if(is_array($shortcuts)) {
-          foreach($shortcuts as $shortcut) {
-            $li = new XMLElement('li');
-            $li->appendChild(new XMLElement('h4', __("Shortcut")));
 
-            $divcontent = new XMLElement('div');
-            $divcontent->setAttribute('class', 'content');
+require_once(TOOLKIT . '/class.sectionmanager.php');
 
-            $divgroup = new XMLElement('div');
-            $divgroup->setAttribute('class', 'group');
+Class extension_publish_shortcuts extends Extension {
 
-            $label = Widget::Label(__('Label'));
-            $label->appendChild(Widget::Input("settings[publish_shortcuts][][label]", General::sanitize($shortcut['label'])));
-            $divgroup->appendChild($label);
+	/*-------------------------------------------------------------------------
+		Extension definition
+	-------------------------------------------------------------------------*/
 
-            $label = Widget::Label(__('Link'));
-            $label->appendChild(Widget::Input("settings[publish_shortcuts][][link]", General::sanitize($shortcut['link'])));
-            $divgroup->appendChild($label);
-            $divcontent->appendChild($divgroup);
+	public function about() {
+		return array(
+			'name' => 'Publish Shortcuts',
+			'version' => '1.1',
+			'release-date' => '2011-03-18',
+			'author' => array(
+				'name' => 'Max Wheeler',
+				'email' => 'max@makenosound.com',
+			),
+			'description' => 'Lets you define shortcut buttons for the section/publish index pages.',
+		);
+	}
 
-            $divgroup = new XMLElement('div');
-            $divgroup->setAttribute('class', 'group');
-            
-            # Set selected
-            foreach ($options as &$option)
-            {
-              $option[1] = ($option[0] == $shortcut['section_id']) ? true : false;
-            }
-            
-            $label = Widget::Label(__('Section'));
-            $label->appendChild(Widget::Select("settings[publish_shortcuts][][section_id]", $options));
-            $divgroup->appendChild($label);
-            $divcontent->appendChild($divgroup);
+	/*-------------------------------------------------------------------------
+	Un/installation
+	-------------------------------------------------------------------------*/
 
-            $li->appendChild($divcontent);
-            $ol->appendChild($li);
-          }
-        }
-      }
-      $group->appendChild($ol);
-      $fieldset->appendChild($group);
-      $context['wrapper']->appendChild($fieldset);
-    }
-    
-    # Add shortcut resources to publish indexs pages
-    public function initaliseAdminPageHead($context) {
-      $page = $context['parent']->Page;
+	public function install() {
+		Symphony::Database()->query("
+			CREATE TABLE IF NOT EXISTS `tbl_publish_shortcuts` (
+			`id` int(11) NOT NULL auto_increment,
+			`label` varchar(255) NOT NULL,
+			`link` varchar(255) NOT NULL,
+			`section_id` int(11) NOT NULL,
+			`sort_order` int(11) NOT NULL,
+			PRIMARY KEY (`id`)
+			)
+		");
+	}
+	
+	public function update($previousVersion) {
+		if(version_compare($previousVersion, '1.1', '<')) {
+			Administration::instance()->Database->query("ALTER TABLE `tbl_publish_shortcuts` CHANGE `order` `sort_order` int(11) NOT NULL");
+		}
+	}
 
-      // Include shortcuts
-      if ($page instanceof ContentPublish and $page->_context['page'] == 'index') {
-        $page->addStylesheetToHead(URL . '/extensions/publish_shortcuts/assets/shortcuts.css', 'screen', 902010);
-        $page->addScriptToHead(URL . '/symphony/extension/publish_shortcuts/shortcuts/?section=' . $page->_context['section_handle'], 90211);
-        $page->addScriptToHead(URL . '/extensions/publish_shortcuts/assets/shortcuts.js', 90212);
-      }
-    }
-    
-    /*-------------------------------------------------------------------------
-      Helpers
-    -------------------------------------------------------------------------*/
-    public function getShortcuts($section_id = null)
-    {
-      $query = "SELECT * FROM tbl_publish_shortcuts";
-      if (isset($section_id)) $query .= " WHERE section_id = $section_id";
-      $shortcuts = $this->_Parent->Database->fetch($query);
-      return $shortcuts;
-    }
-    
-    private function getSections()
-    {
-      $raw = $this->_sectionManager->fetch();
-      foreach ($raw as $section)
-      {
-        $sections[] = array(
-          'id' => $section->_data['id'],
-          'name' => $section->_data['name']
-        );
-      }
-      return $sections;
-    }
-    
-    # Save the shortcuts
-    public function save(&$context) {
-      # Setup
-      $shortcuts = array();
-      $shortcut = array();
-      $count = 0;
-      
-      # Munge data into cleaner structure
-      if (isset($context['settings']['publish_shortcuts'])) {
-        foreach($context['settings']['publish_shortcuts'] as $item)
-        {
-          if (isset($item['label'])) {
-            $shortcut['label'] = $item['label'];
-          } else if(isset($item['link'])) {
-            $shortcut['link'] = $item['link'];
-          } else if(isset($item['section_id'])) {
-            $shortcut['section_id'] = $item['section_id'];
-            $shortcut['order'] = $count;
-            $shortcuts[] = $shortcut;
-            $count++;
-            $shortcut = array();
-          }
-        }
-      }
-      
-      # Clean up
-      $this->_Parent->Database->query("DELETE FROM tbl_publish_shortcuts");
-      if (isset($context['settings']['publish_shortcuts'])) $this->_Parent->Database->insert($shortcuts, "tbl_publish_shortcuts");
-      unset($context['settings']['publish_shortcuts']);
-    }
-  }
+	public function uninstall() {
+		Symphony::Database()->query("DROP TABLE `tbl_publish_shortcuts`");
+	}
+
+	/*-------------------------------------------------------------------------
+		Delegates and callbacks
+	-------------------------------------------------------------------------*/
+
+	public function getSubscribedDelegates() {
+		return array(
+			array(
+				'page'      => '/system/preferences/',
+				'delegate'  => 'AddCustomPreferenceFieldsets',
+				'callback'  => 'addCustomPreferenceFieldsets'
+			),
+			array(
+				'page'      => '/backend/',
+				'delegate'  => 'InitaliseAdminPageHead',
+				'callback'  => 'initaliseAdminPageHead'
+			),
+			array(
+				'page'      => '/system/preferences/',
+				'delegate'  => 'Save',
+				'callback'  => 'save'
+			)
+		);
+	}
+
+	/**
+	 * Add duplicator to System Preferences page, lists existing
+	 * shortcuts as well as templates for each section
+	 */
+	public function addCustomPreferenceFieldsets(&$context) {
+		
+		$sm = new SectionManager(Administration::instance());
+		$sections = $sm->fetch();
+		
+		$fieldset = new XMLElement('fieldset');
+		$fieldset->setAttribute('class', 'settings');
+		$fieldset->appendChild(new XMLElement('legend', __('Publish Shortcuts')));
+
+		$p = new XMLElement('p', __('Shortcut links for Section index pages'));
+		$p->setAttribute('class', 'help');
+		$fieldset->appendChild($p);
+		
+		$group = new XMLElement('div');
+		$group->setAttribute('class', 'subsection');
+		$group->appendChild(new XMLElement('span', __('Shortcuts Links')));
+		
+		$div = new XMLElement('div');
+		$div->setAttribute('class', 'contextual');
+		$h3 = new XMLElement('p', __('Filter'), array('class' => 'label'));
+		$h3->setAttribute('class', 'label');
+		$div->appendChild($h3);
+		
+		$ol = new XMLElement('ol');
+		$ol->setAttribute('id', 'fields-duplicator');
+		
+		$shortcuts = $this->__getShortcuts();
+		if(!is_array($shortcuts)) $shortcuts = array($shortcuts);
+		
+		if(is_array($sections)) {
+			foreach($sections as $section){
+				
+				foreach($shortcuts as $shortcut) {
+					if($shortcut['section_id'] != $section->get('id')) continue;
+					$wrapper = $this->__buildDuplicatorItem($section, $shortcut);
+					$ol->appendChild($wrapper);
+				}
+				
+				$wrapper = $this->__buildDuplicatorItem($section, NULL);
+				$ol->appendChild($wrapper);
+				
+			}
+		}
+		
+		$div->appendChild($ol);
+		
+		$group->appendChild($ol);
+		$fieldset->appendChild($group);
+		$context['wrapper']->appendChild($fieldset);
+		
+	}
+	
+	/**
+	 * Returns a list item for the duplicator. When no shortcut is passed
+	 * a section template is returned, rather than an existing duplicator item
+	 */
+	private function __buildDuplicatorItem($section, $shortcut=NULL) {
+		
+		$wrapper = new XMLElement('li');
+		$wrapper->setAttribute('class', ($shortcut == NULL) ? 'template' : '');
+		
+		$wrapper->appendChild(new XMLElement('h4', $section->get('name')));
+		
+		$divgroup = new XMLElement('div');
+		$divgroup->setAttribute('class', 'group');
+		
+		$index = ($shortcut == NULL) ? '-1' : $shortcut['id'];
+
+		$label = Widget::Label(__('Label'));
+		$label->appendChild(Widget::Input("settings[publish_shortcuts][".$index."][label]", General::sanitize($shortcut['label'])));
+		$divgroup->appendChild($label);
+
+		$label = Widget::Label(__('Link') . '<i>' . __('Prefix with {$root} for absolute URLs') . '</i>');
+		$label->appendChild(Widget::Input("settings[publish_shortcuts][".$index."][link]", General::sanitize($shortcut['link'])));
+		$divgroup->appendChild($label);
+		
+		$wrapper->appendChild(new XMLElement('input', NULL, array('type' => 'hidden', 'name' => 'settings[publish_shortcuts]['.$index.'][section_id]', 'value' => $section->get('id'))));
+		
+		$wrapper->appendChild($divgroup);
+		
+		return $wrapper;
+		
+	}
+
+	/**
+	 * Add shortcuts to page DOM
+	 */
+	public function initaliseAdminPageHead($context) {
+		$page = $context['parent']->Page;
+		
+		if ($page instanceof ContentPublish and $page->_context['page'] == 'index') {
+			
+			$sm = new SectionManager(Administration::instance());
+			$section_id = $sm->fetchIDFromHandle($page->_context['section_handle']);
+			$shortcuts = $this->__getShortcuts($section_id);
+			
+			$page->addElementToHead(new XMLElement(
+				'script',
+				"Symphony.Context.add('publish_shortcuts', " . json_encode($shortcuts) . ")",
+				array('type' => 'text/javascript')
+			), 902011);
+			
+			$page->addScriptToHead(URL . '/extensions/publish_shortcuts/assets/publish_shortcuts.publish.js', 90211);
+		}
+	}
+	
+	/**
+	 * Save shortcuts when System Preferences page is submitted
+	 */
+	public function save(&$context) {
+		
+		Symphony::Database()->query("DELETE FROM tbl_publish_shortcuts");
+		
+		$shortcuts = $context['settings']['publish_shortcuts'];
+		unset($context['settings']['publish_shortcuts']);
+		
+		if (!isset($shortcuts)) return;
+		
+		foreach($shortcuts as $i => $shortcut) {
+			if($shortcut['label'] == '' || $shortcut['link'] == '') continue;
+			$shortcut['sort_order'] = $i;
+			Symphony::Database()->insert($shortcut, "tbl_publish_shortcuts");
+		}
+
+	}
+	
+	/*-------------------------------------------------------------------------
+		Helpers
+	-------------------------------------------------------------------------*/
+
+	/**
+	 * Helper: returns all shortcuts, optionally filtered by a section ID
+	 */
+	private function __getShortcuts($section_id=NULL) {
+		return Symphony::Database()->fetch(sprintf(
+			"SELECT * FROM tbl_publish_shortcuts %s ORDER BY `sort_order` ASC",
+			(is_null($section_id)) ? '' : "WHERE section_id = $section_id"
+		));
+	}
+	
+}
